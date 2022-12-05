328e210849cde1c016cf13eab4c64f5322fd7d06
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-12-05 15:24:34 +0200

    feat: use Astro components instead of Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://eetumaenpaa.fi",
-  integrations: [svelte(), mdx()],
+  integrations: [mdx()],
 });
