3a3cf3c3edcddabcce621f4dbcc66aafaeb55b79
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-02-19 19:42:03 +0200

    refactor: use Astro content collections

---
+++ src/content/config.ts
@@ -0,0 +1,27 @@
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    releaseDate: z.string(),
+    excerpt: z.string()
+  })
+});
+
+const projectCollection = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    blogTitle: z.string(),
+    releaseDate: z.string(),
+    description: z.string(),
+    links: z.array(z.object({
+      href: z.string(),
+      title: z.string()
+    }))
+  })
+});
+
+export const collections = {
+  blog: blogCollection,
+  project: projectCollection
+}
